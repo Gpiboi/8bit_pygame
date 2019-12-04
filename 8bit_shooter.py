e8493b490905482be7954466630cc9af3003f6f2
NazeerGreene <58005647+NazeerGreene@users.noreply.github.com>
2019-12-04 15:12:07 -0800

    Feature - Bullets
    - Space bar button to shoot bullets
    - bullet list
    - checks to see if bullets are off screen
    - bullets appear on screen

--- 8bit_shooter.py
+++ 8bit_shooter.py
@@ -16,6 +16,7 @@ WINDOW_WIDTH = 900
 WINDOW_HEIGHT = 500
 FRAMES_PER_SECOND = 30
 ENEMY_MAX = 5
+DEBUG = True
 
 
 # 3 - Initialize the world
@@ -23,6 +24,7 @@ pygame.init()
 window = pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
 clock = pygame.time.Clock()
 pygame.display.set_caption("ARE YOU READY PLAYER ONE?")
+
  
 # 4 - Load assets: image(s), sounds,  etc.
 background = pygwidgets.Image(window, (0,0), "pictures/8bit_background.jpg")
@@ -38,6 +40,8 @@ advances.start()
 enemy_x_cord_start = 200
 enemies = []
 
+pBullets = []
+
 
 # 6 - Loop forever
 while True:
@@ -45,9 +49,14 @@ while True:
     # 7 - Check for and handle events
     for event in pygame.event.get():
         # If the event was a click on the close box, quit pygame and the program 
-        if event.type == pygame.QUIT:           
+        if event.type == pygame.QUIT:        
             pygame.quit()  
             sys.exit()
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                pBullets.append( Bullet(window, playerOne.x) )
+                
 
     keyList = pygame.key.get_pressed()
 
@@ -83,9 +92,22 @@ while True:
         
     playerOne.draw()
 
+    for bullet in pBullets:
+        bullet.update()
+        bullet.draw()
+        
+        if bullet.offScreen:
+            pBullets.remove(bullet)
+            del bullet
+            
+
+    if DEBUG:   print(len(pBullets))
+    
     
     # 11 - Update the screen
     pygame.display.update()
     
     # 12 - Slow things down a bit
     clock.tick(FRAMES_PER_SECOND)  # make PyGame wait the correct amount
+
+
